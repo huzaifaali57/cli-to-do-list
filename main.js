d16b01b2eb270d80595c34f9d907f90ae5d4b3b5
huzaifaali57 <syedhuzaifa6412@gmail.com>
2024-04-03 17:23:16 +0500

    Added functionalities "Add,Delete,Update & Exit" to List

--- main.js
+++ main.js
@@ -2,21 +2,63 @@
 import inquirer from "inquirer";
 let todos = [];
 let condition = true;
+let answers = await inquirer.prompt([
+    {
+        name: "whattodo",
+        type: "input",
+        message: "What do you want to add in your to do list?",
+    },
+]);
+todos.push(answers.whattodo);
 while (condition) {
-    let answers = await inquirer.prompt([
+    answers = await inquirer.prompt([
         {
-            name: "whattodo",
-            type: "input",
-            message: "What do you want to add in your to do list?",
-        },
-        {
-            name: "addtolist",
-            type: "confirm",
-            message: "Do you want to add more?",
-            default: "false",
+            name: "listOptions",
+            type: "list",
+            message: "What changes do you like to make :",
+            choices: ["Add to List", "Delete from List", "Update List", "Exit"],
         },
     ]);
-    todos.push(answers.whattodo);
-    console.log(todos);
-    condition = answers.addtolist;
+    if (answers.listOptions == "Add to List") {
+        let addtolist = await inquirer.prompt([
+            {
+                name: "whattoadd",
+                type: "input",
+                message: "What do you want to add in your to do list?",
+            },
+        ]);
+        todos.push(addtolist.whattoadd);
+        console.log(todos);
+    }
+    else if (answers.listOptions == "Delete from List") {
+        let deletefromlist = await inquirer.prompt([
+            {
+                name: "whattodelete",
+                type: "input",
+                message: "Which index number do you want to delete",
+            },
+        ]);
+        todos.splice(deletefromlist.whattodelete, 1);
+        console.log(todos);
+    }
+    else if (answers.listOptions == "Update List") {
+        let updatelist = await inquirer.prompt([
+            {
+                name: "indextoupdate",
+                type: "input",
+                message: "Which index number you want to update ?",
+            },
+            {
+                name: "whattoupdate",
+                type: "input",
+                message: "What do you want to update ?",
+            },
+        ]);
+        todos.splice(updatelist.indextoupdate, 1, updatelist.whattoupdate);
+        console.log(todos);
+    }
+    else if (answers.listOptions == "Exit") {
+        condition = false;
+    }
+    console.log(`Your List \n${todos}\n`);
 }
